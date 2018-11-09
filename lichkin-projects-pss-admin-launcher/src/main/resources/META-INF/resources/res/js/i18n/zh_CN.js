8eb80500d46d1b6784abc9245316238e5bca697e
China-zhuangxuxin <zhuangxuxin@hotmail.com>
2018-11-09 09:37:36 +0800

    lanucher脚本

--- lichkin-projects-pss-admin-launcher/src/main/resources/META-INF/resources/res/js/i18n/zh_CN.js
+++ lichkin-projects-pss-admin-launcher/src/main/resources/META-INF/resources/res/js/i18n/zh_CN.js
@@ -737,6 +737,11 @@ $.LKExtendI18N({
         'remove' : '数据删除后将不能恢复，确认删除么？',
         'submit' : '数据提交后将不能修改，确认提交么？',
       },
+
+      'product not exists' : '产品不存在，请先录入产品。',
+      'only PENDING status can be edit' : '只有草稿状态才可编辑。',
+      'only PENDING status can be remove' : '只有草稿状态才可删除。',
+      'only PENDING status can be submit' : '只有草稿状态才可提交。',
     },
 
     'product-grid' : {
@@ -757,7 +762,6 @@ $.LKExtendI18N({
       'remove' : '删除产品',
     },
 
-    'product not exists' : '产品不存在，请先录入产品。',
   }
 });
 $.LKExtendI18N({
@@ -822,8 +826,54 @@ $.LKExtendI18N({
     'grid' : {
       'title' : '调拨单',
 
-      'columns' : {},
-    }
+      'columns' : {
+        'orderNo' : '订单编号',
+        'billDate' : '调拨日期',
+        'outStorageName' : '调出仓库',
+        'inStorageName' : '调入仓库',
+        'approvalStatus' : '审批状态',
+        'approvalTime' : '审批时间',
+        'startDate' : '订单开始日期',
+        'endDate' : '订单结束日期',
+        'scanCode' : '扫码',
+        'remarks' : '备注',
+      },
+
+      'add' : '录入订单',
+      'edit' : '编辑订单',
+      'remove' : '删除订单',
+      'submit' : '提交订单',
+      'view' : '查看订单',
+
+      'confirm' : {
+        'remove' : '数据删除后将不能恢复，确认删除么？',
+        'submit' : '数据提交后将不能修改，确认提交么？',
+      },
+
+      'please choose the storage' : '请选择调出仓库。',
+      'product not exists' : '产品不存在，请先录入产品。',
+      'only PENDING status can be edit' : '只有草稿状态才可编辑。',
+      'only PENDING status can be remove' : '只有草稿状态才可删除。',
+      'only PENDING status can be submit' : '只有草稿状态才可提交。',
+    },
+
+    'product-grid' : {
+      'title' : '产品列表',
+
+      'columns' : {
+        'productCode' : '产品编码',
+        'productName' : '产品名称',
+        'barcode' : '条形码',
+        'unit' : '单位',
+        'stockQuantity' : '库存数量',
+        'quantity' : '调拨数量',
+        'product' : '选择产品',
+      },
+
+      'add' : '录入产品',
+      'remove' : '删除产品',
+    },
+
   }
 });
 $.LKExtendI18N({
@@ -833,30 +883,163 @@ $.LKExtendI18N({
     'grid' : {
       'title' : '盘点单',
 
-      'columns' : {},
-    }
+      'columns' : {
+        'orderNo' : '订单编号',
+        'billDate' : '盘点日期',
+        'storageName' : '仓库',
+        'scanCode' : '扫码',
+        'stockCheckCount' : '盘点产品数',
+        'approvalStatus' : '盘点状态',
+        'approvalTime' : '审批时间',
+        'startDate' : '订单开始日期',
+        'endDate' : '订单结束日期',
+        'scanCode' : '扫码',
+        'remarks' : '备注',
+      },
+
+      'add' : '录入订单',
+      'edit' : '编辑订单',
+      'remove' : '删除订单',
+      'submit' : '提交订单',
+      'view' : '查看订单',
+
+      'confirm' : {
+        'remove' : '数据删除后将不能恢复，确认删除么？',
+        'submit' : '数据提交后将不能修改，确认提交么？',
+      },
+
+      'please choose the storage' : '请选择盘点仓库。',
+      'product not exists' : '产品不存在，请先录入产品。',
+      'only PENDING status can be edit' : '只有草稿状态才可编辑。',
+      'only PENDING status can be remove' : '只有草稿状态才可删除。',
+      'only PENDING status can be submit' : '只有草稿状态才可提交。',
+    },
+
+    'product-grid' : {
+      'title' : '产品列表',
+
+      'columns' : {
+        'productCode' : '产品编码',
+        'productName' : '产品名称',
+        'barcode' : '条形码',
+        'unit' : '单位',
+        'quantity' : '盘点数量',
+        'stockQuantity' : '系统数量',
+        'differenceQuantity' : '盘盈盘亏',
+        'product' : '选择产品',
+      },
+
+      'add' : '录入产品',
+      'remove' : '删除产品',
+    },
+    
+    
   }
 });
 $.LKExtendI18N({
   'otherStockInOrder' : {
-    'title' : '其它入库单',
+    'title' : '其他入库单',
 
     'grid' : {
-      'title' : '其它入库单',
+      'title' : '其他入库单',
 
-      'columns' : {},
-    }
+      'columns' : {
+        'orderNo' : '订单号',
+        'storageType' : '入库类型',
+        'storageName' : '仓库',
+        'billDate' : '订单日期',
+        'approvalStatus' : '审批状态',
+        'approvalTime' : '审批时间',
+        'startDate' : '订单开始日期',
+        'endDate' : '订单结束日期',
+        'scanCode' : '扫码',
+        'remarks' : '备注',
+      },
+
+      'add' : '录入订单',
+      'edit' : '编辑订单',
+      'remove' : '删除订单',
+      'submit' : '提交订单',
+      'view' : '查看订单',
+
+      'confirm' : {
+        'remove' : '数据删除后将不能恢复，确认删除么？',
+        'submit' : '数据提交后将不能修改，确认提交么？',
+      },
+
+      'product not exists' : '产品不存在，请先录入产品。',
+      'only PENDING status can be edit' : '只有草稿状态才可编辑。',
+      'only PENDING status can be remove' : '只有草稿状态才可删除。',
+      'only PENDING status can be submit' : '只有草稿状态才可提交。',
+    },
+
+    'product-grid' : {
+      'title' : '产品列表',
+
+      'columns' : {
+        'productCode' : '产品编码',
+        'productName' : '产品名称',
+        'barcode' : '条形码',
+        'unit' : '单位',
+        'quantity' : '数量',
+        'product' : '选择产品',
+      }
+    },
+
   }
 });
 $.LKExtendI18N({
   'otherStockOutOrder' : {
-    'title' : '其它出库单',
+    'title' : '其他出库单',
 
     'grid' : {
-      'title' : '其它出库单',
+      'title' : '其他出库单',
 
-      'columns' : {},
-    }
+      'columns' : {
+        'orderNo' : '订单号',
+        'storageType' : '出库类型',
+        'storageName' : '仓库',
+        'billDate' : '订单日期',
+        'approvalStatus' : '审批状态',
+        'approvalTime' : '审批时间',
+        'startDate' : '订单开始日期',
+        'endDate' : '订单结束日期',
+        'scanCode' : '扫码',
+        'remarks' : '备注',
+      },
+
+      'add' : '录入订单',
+      'edit' : '编辑订单',
+      'remove' : '删除订单',
+      'submit' : '提交订单',
+      'view' : '查看订单',
+
+      'confirm' : {
+        'remove' : '数据删除后将不能恢复，确认删除么？',
+        'submit' : '数据提交后将不能修改，确认提交么？',
+      },
+
+      'please select the storage first' : '请先选择仓库。',
+      'product not exists' : '产品不存在，请先录入产品。',
+      'only PENDING status can be edit' : '只有草稿状态才可编辑。',
+      'only PENDING status can be remove' : '只有草稿状态才可删除。',
+      'only PENDING status can be submit' : '只有草稿状态才可提交。',
+    },
+
+    'product-grid' : {
+      'title' : '产品列表',
+
+      'columns' : {
+        'productCode' : '产品编码',
+        'productName' : '产品名称',
+        'barcode' : '条形码',
+        'unit' : '单位',
+        'stockQuantity' : '库存数量',
+        'quantity' : '数量',
+        'product' : '选择产品',
+      }
+    },
+
   }
 });
 $.LKExtendI18N({
