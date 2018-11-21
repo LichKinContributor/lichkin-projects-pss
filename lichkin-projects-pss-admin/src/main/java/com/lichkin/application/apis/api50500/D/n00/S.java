98761d49a126f0168a5d318521cd1007f666a4e9
lk-chenshuili <chenshuili@lichkin.com>
2018-11-21 15:06:59 +0800

    盘点单只有STAND_BY状态可删除

--- lichkin-projects-pss-admin/src/main/java/com/lichkin/application/apis/api50500/D/n00/S.java
+++ lichkin-projects-pss-admin/src/main/java/com/lichkin/application/apis/api50500/D/n00/S.java
@@ -5,8 +5,8 @@ import org.springframework.stereotype.Service;
 
 import com.lichkin.application.services.bus.impl.SysPssStockCheckOrderBusService;
 import com.lichkin.framework.db.beans.SysPssStockCheckOrderR;
-import com.lichkin.framework.defines.enums.impl.ApprovalStatusEnum;
 import com.lichkin.framework.defines.enums.impl.LKErrorCodesEnum;
+import com.lichkin.framework.defines.enums.impl.LKUsingStatusEnum;
 import com.lichkin.framework.defines.exceptions.LKRuntimeException;
 import com.lichkin.springframework.entities.impl.SysPssStockCheckOrderEntity;
 import com.lichkin.springframework.services.LKApiBusDeleteService;
@@ -32,9 +32,9 @@ public class S extends LKApiBusDeleteService<I, SysPssStockCheckOrderEntity> {
 
 	@Override
 	protected void beforeRealDelete(I sin, String locale, String compId, String loginId, SysPssStockCheckOrderEntity entity, String id) {
-		ApprovalStatusEnum approvalStatus = entity.getApprovalStatus();
-		switch (approvalStatus) {
-			case PENDING:
+		LKUsingStatusEnum usingStatus = entity.getUsingStatus();
+		switch (usingStatus) {
+			case STAND_BY:
 				busService.clearPssStockCheckOrderProduct(id);
 			break;
 			default:
