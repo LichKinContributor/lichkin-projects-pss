46c65e962a3215157d759b082f1e4d553a888aee
China-zhuangxuxin <zhuangxuxin@hotmail.com>
2018-11-23 09:29:16 +0800

    盘点单取消流程审批

--- lichkin-projects-pss-services/src/main/java/com/lichkin/application/services/impl/activiti/ActivitiCallbackService_PSS_STOCK_CHECK_ORDER.java
+++ lichkin-projects-pss-services/src/main/java/com/lichkin/application/services/impl/activiti/ActivitiCallbackService_PSS_STOCK_CHECK_ORDER.java
@@ -5,20 +5,18 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.lichkin.application.services.ActivitiCallbackService;
 import com.lichkin.application.services.WithoutActivitiCallbackService;
 import com.lichkin.application.services.bus.impl.SysPssStockBusService;
 import com.lichkin.defines.PssStatics;
 import com.lichkin.framework.db.beans.QuerySQL;
 import com.lichkin.framework.db.beans.SysPssStockCheckOrderProductR;
 import com.lichkin.framework.defines.enums.impl.LKUsingStatusEnum;
-import com.lichkin.springframework.entities.impl.SysActivitiFormDataEntity;
 import com.lichkin.springframework.entities.impl.SysPssStockCheckOrderEntity;
 import com.lichkin.springframework.entities.impl.SysPssStockCheckOrderProductEntity;
 import com.lichkin.springframework.services.LKDBService;
 
 @Service(PssStatics.PSS_STOCK_CHECK_ORDER)
-public class ActivitiCallbackService_PSS_STOCK_CHECK_ORDER extends LKDBService implements ActivitiCallbackService, WithoutActivitiCallbackService<SysPssStockCheckOrderEntity> {
+public class ActivitiCallbackService_PSS_STOCK_CHECK_ORDER extends LKDBService implements WithoutActivitiCallbackService<SysPssStockCheckOrderEntity> {
 
 	@Autowired
 	private SysPssStockBusService stockBusService;
@@ -32,22 +30,6 @@ public class ActivitiCallbackService_PSS_STOCK_CHECK_ORDER extends LKDBService i
 		sql.eq(SysPssStockCheckOrderProductR.orderId, processEntity.getId());
 		List<SysPssStockCheckOrderProductEntity> orderProductList = dao.getList(sql, SysPssStockCheckOrderProductEntity.class);
 		stockBusService.changeStockQuantityByCheckOrder(processEntity, orderProductList);
-	}
-
-
-	@Override
-	public void approve(SysActivitiFormDataEntity formDataEntity, byte step) {
-	}
-
-
-	@Override
-	public void finish(SysActivitiFormDataEntity formDataEntity) {
-		directFinish(dao.findOneById(SysPssStockCheckOrderEntity.class, formDataEntity.getField1()), formDataEntity.getCompId(), formDataEntity.getApproverLoginId());
-	}
-
-
-	@Override
-	public void reject(SysActivitiFormDataEntity formDataEntity) {
 	}
 
 }
